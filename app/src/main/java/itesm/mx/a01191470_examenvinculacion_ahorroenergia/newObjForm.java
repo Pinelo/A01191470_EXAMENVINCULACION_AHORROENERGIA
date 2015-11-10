73cb5508000219456d10aac0e8685e347bd71f7a
hugo <a01191470@itesm.mx>
2015-11-10 12:34:50 -0600

    Camera takes pictures when prompted and the picture is displayed.

--- app/src/main/java/itesm/mx/a01191470_examenvinculacion_ahorroenergia/newObjForm.java
+++ app/src/main/java/itesm/mx/a01191470_examenvinculacion_ahorroenergia/newObjForm.java
@@ -1,15 +1,27 @@
 package itesm.mx.a01191470_examenvinculacion_ahorroenergia;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 public class newObjForm extends AppCompatActivity {
 
     TextView nameTv;
+    ImageView cameraIV;
+    NumberPicker hoursNP;
+    Button takePicBT;
+    static final int REQUEST_IMAGE_CAPTURE= 1;
+
 
 
     @Override
@@ -18,12 +30,49 @@ public class newObjForm extends AppCompatActivity {
         setContentView(R.layout.activity_new_obj_form);
 
         nameTv = (TextView)findViewById(R.id.deviceNameTV);
+        cameraIV = (ImageView)findViewById(R.id.cameraIV);
+        takePicBT = (Button)findViewById(R.id.picBT);
+        hoursNP = (NumberPicker)findViewById(R.id.hoursNP);
+
+        hoursNP.setMaxValue(1000);
+        hoursNP.setMinValue(0);
+        hoursNP.setWrapSelectorWheel(false);
 
         Intent intent = getIntent();
         String deviceName = intent.getStringExtra("name");
         int deviceConsumption = intent.getIntExtra("consumption", 0);
-
         nameTv.setText(deviceName);
+
+        takePicBT.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        launchCamera(v);
+                    }
+                }
+        );
+
+    }
+
+//    checa si el usuario tiene una camara
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+//    toma foto
+    public void launchCamera(View view) {
+        Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap)extras.get("data");
+            cameraIV.setImageBitmap(photo);
+        }
 
     }
 
